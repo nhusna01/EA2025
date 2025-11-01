072f64ac9b7e1f0f2eeb28d6aac93e1c3dfb0692
nhusna01 <nrlhsn1310@gmail.com>
2025-11-01 23:18:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Academic"
+)
+visualise = st.Page('Academic.py', title='Student Coursework', icon=":material/school:")
+
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+
+pg = st.navigation(
+        {
+            "Menu": [home, visualise]
+        }
+    )
+
+pg.run()
