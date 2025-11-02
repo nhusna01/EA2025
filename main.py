8722352c2125b685a31d88a418bbb89311078d2d
nhusna01 <nrlhsn1310@gmail.com>
2025-11-02 11:30:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import streamlit as st
 
-# Page configuration
 st.set_page_config(page_title="Academic")
 
-# Define your pages
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
-visualise = st.Page('Academic.py', title='Student Coursework', icon=":material/school:")
+academic = st.Page('Academic.py', title='Student Coursework', icon=":material/school:")
 visual1 = st.Page('Visualization1.py', title='Visualization 1', icon=":material/bar_chart:")
 visual2 = st.Page('Visualization2.py', title='Visualization 2', icon=":material/insights:")
 visual3 = st.Page('Visualization3.py', title='Visualization 3', icon=":material/analytics:")
 
-# Navigation menu
-pg = st.navigation(
-    {
-        "Menu": [home, visualise],
-        "Visualizations": [visual1, visual2, visual3]
-    }
-)
+pg = st.navigation({
+    "Menu": [home, academic],
+    "Visualizations": [visual1, visual2, visual3]
+})
 
-# Run the navigation
 pg.run()
